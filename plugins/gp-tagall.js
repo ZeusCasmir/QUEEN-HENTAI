f5bb9738cbfac9c50d68122a112708d07f21d292
ZeusCasmir <139172877+ZeusCasmir@users.noreply.github.com>
2023-11-20 00:08:14 +0100

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`🔊 Group : *${groupMetadata.subject}*\n🔊 Members : *${participants.length}*${text ? `\n🔊 Message : ${text}\n` : ''}\n┌───⊷ *Mention By 💝 Queen Hentai 💝*\n` + users.map(v => '💝 @' + v.replace(/@.+/, '')).join`\n` + '\n└──*💝 Queen Hentai 💝*──', null, {
+    m.reply(`🔊 Group : *${groupMetadata.subject}*\n🔊 Members : *${participants.length}*${text ? `\n🔊 Message : ${text}\n` : ''}\n┌──Let's Go ⚡*\n` + users.map(v => '_ @' + v.replace(/@.+/, '')).join`\n` + '\n└──* ✅ *──', null, {
         mentions: users
     })
 }
